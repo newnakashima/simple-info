9757a628b6c475aa856e4a053eb3c01996cd854a
newnakashima <newnakashima2014@gmail.com>
2017-04-02 21:34:53 +0900

    initial commit

---
+++ src/Balloon.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Balloon extends Component { 
+  constructor(props) {
+    super(props);
+  }
+  render() {
+      return (
+          <h1>This is test.</h1>
+      );
+  }
+}
+
+export default Balloon;
